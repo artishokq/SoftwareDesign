839de60bd8dec32152162b25f2de293c3482c436
artishokq <astkachuk_2@edu.hse.ru>
2025-05-21 20:55:20 +0200

    еще чуть-чуть

--- APIGateway/Program.cs
+++ APIGateway/Program.cs
@@ -1,9 +1,7 @@
 ﻿using Microsoft.AspNetCore.Builder;
-using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
 using Microsoft.OpenApi.Models;
-using Yarp.ReverseProxy;
 
 var builder = WebApplication.CreateBuilder(args);
 
